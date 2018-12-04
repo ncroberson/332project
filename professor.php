972cf8cadefdc702b01f64d1856aefca5ed6874a
Noah Roberson <ncroberson@outlook.com>
2018-12-03 16:46:22 -0800

    added test implementation for professor_action.php

--- professor.php
+++ professor.php
@@ -3,7 +3,7 @@
 <body>
 <h4><u> Professor </u></h4>
 
-<form action="/action/professor/professor_action.php">
+<form action="action/professor/professor_action.php">
 <input type="submit" value="Test">
 </form>
 </body>
